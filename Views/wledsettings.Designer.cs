1c1706da503ee4be3d81352b46a2810f51371111
jimmy <jimmy@deviousweb.com>
2023-02-27 19:21:47 +0000

    wled settings done

--- Views/wledsettings.Designer.cs
+++ Views/wledsettings.Designer.cs
@@ -63,24 +63,23 @@
             this.btn_stop.TabIndex = 1;
             this.btn_stop.Text = "Stop";
             this.btn_stop.UseVisualStyleBackColor = true;
-            this.btn_stop.Click += new System.EventHandler(this.btn_discover_Click);
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
             // 
             // statusStrip1
             // 
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 108);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 112);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(402, 26);
+            this.statusStrip1.Size = new System.Drawing.Size(402, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
-            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 16);
             // 
             // wledsettings
             // 
--- Views/wledsettings.cs
+++ Views/wledsettings.cs
@@ -9,14 +9,18 @@ using THFHA_V1._0.Model;
 
 namespace THFHA_V1._0.Views
 {
- 
+
+
+
     public partial class wledsettings : Form
     {
         private Settings settings;
+        private WLED _selectedWled;
+        private bool isDiscovering = false;
         public event EventHandler<WLED>? DeviceDiscovered;
         private BindingList<WLED> _wledLights = new BindingList<WLED>();
         public List<WLED> WledLights { get { return _wledLights.OfType<WLED>().ToList(); } }
-        
+
         public wledsettings()
         {
             InitializeComponent();
@@ -24,6 +28,11 @@ namespace THFHA_V1._0.Views
             listbox_wledlights.DataSource = null;
             listbox_wledlights.DataSource = _wledLights;
             // Load the selected item from the settings object and select it in the listbox.
+            if (_selectedWled != null)
+            {
+                listbox_wledlights.SelectedValue = _selectedWled.Ip + ":" + _selectedWled.Port;
+            }
+
             var selectedItem = this.settings.SelectedWled;
             if (settings.WledDevices != null)
             {
@@ -38,7 +47,7 @@ namespace THFHA_V1._0.Views
                     if (wled.Ip == selectedItem.Ip && wled.Port == selectedItem.Port)
                     {
                         listbox_wledlights.SelectedItem = wled;
-                        listbox_wledlights.SelectedValue = wled.Ip + ":" + wled.Port; // set the SelectedValue property
+
                         break;
                     }
                 }
@@ -48,96 +57,126 @@ namespace THFHA_V1._0.Views
 
         private async void btn_discover_Click(object sender, EventArgs e)
         {
-            Log.Information("Starting Discovery of WLED lights");
-            toolStripStatusLabel1.Text = "Discovering Lights";
-            // Clear the existing list of WLED lights.
-            _wledLights.Clear();
+            if (isDiscovering)
+            {
+                return;
+            }
+
+            isDiscovering = true;
 
             try
             {
-                var results = await ZeroconfResolver.ResolveAsync("_wled._tcp.local.");
-                foreach (var result in results)
+                Log.Information("Starting Discovery of WLED lights");
+                toolStripStatusLabel1.Text = "Discovering Lights";
+                // Clear the existing list of WLED lights.
+                _wledLights.Clear();
+
+                try
                 {
-                    var host = result.DisplayName;
-                    var ipAddress = result.IPAddress;
-                    var port = result.Services.FirstOrDefault(x => x.Key == "_wled._tcp").Value?.Port ?? 80;
-
-                    var wled = new WLED()
+                    var results = await ZeroconfResolver.ResolveAsync("_wled._tcp.local.");
+                    foreach (var result in results)
                     {
-                        Name = host,
-                        Ip = ipAddress,
-                        Port = port
-                    };
+                        var host = result.DisplayName;
+                        var ipAddress = result.IPAddress;
+                        var port = result.Services.FirstOrDefault(x => x.Key == "_wled._tcp").Value?.Port ?? 80;
 
-                    var client = new WLedClient($"http://{wled.Ip}:{wled.Port}");
-                    var wLedRootResponse = client.Get();
-
-                    if (wLedRootResponse.Status != null)
-                    {
-                        HttpClient myclient = new HttpClient();
-                        try
+                        var wled = new WLED()
                         {
-                            HttpResponseMessage response = await myclient.GetAsync($"http://{wled.Ip}/json");
+                            Name = host,
+                            Ip = ipAddress,
+                            Port = port
+                        };
 
-                            if (response.IsSuccessStatusCode)
+                        var client = new WLedClient($"http://{wled.Ip}:{wled.Port}");
+                        var wLedRootResponse = client.Get();
+
+                        if (wLedRootResponse.Status != null)
+                        {
+                            HttpClient myclient = new HttpClient();
+                            try
                             {
-                                string json = await response.Content.ReadAsStringAsync();
-                                try
+                                HttpResponseMessage response = await myclient.GetAsync($"http://{wled.Ip}/json");
+
+                                if (response.IsSuccessStatusCode)
                                 {
-                                    dynamic jsonObject = JsonConvert.DeserializeObject(json);
-                                    string name = jsonObject.name;
-                                    wled.Name = host;
-                                    wled.Ip = ipAddress;
-                                    wled.Port = port;
-                                    if (!_wledLights.Contains(wled))
+                                    string json = await response.Content.ReadAsStringAsync();
+                                    try
                                     {
-                                        Log.Information("Found a new Wled light called {name}!", wled.Name);
-                                        _wledLights.Add(wled);
-                                        _wledLights = new BindingList<WLED>(_wledLights.OrderBy(wled => wled.Name).Distinct().ToList());
-
-                                        // Update the DataSource property of the listbox control to reflect the changes in the _wledLights list.
-                                        listbox_wledlights.DataSource = null;
-                                        listbox_wledlights.DataSource = _wledLights;
-                                        listbox_wledlights.DisplayMember = "Name";
-
-                                        // Select the stored selected item in the listbox.
-                                        var selectedItem = this.settings.SelectedWled;
-                                        if (selectedItem != null && selectedItem.Ip == wled.Ip && selectedItem.Port == wled.Port)
+                                        dynamic jsonObject = JsonConvert.DeserializeObject(json);
+                                        string name = jsonObject.name;
+                                        wled.Name = host;
+                                        wled.Ip = ipAddress;
+                                        wled.Port = port;
+                                        if (!_wledLights.Contains(wled))
                                         {
-                                            listbox_wledlights.SelectedItem = wled;
-                                        }
+                                            Log.Information("Found a new Wled light called {name}!", wled.Name);
+                                            _wledLights.Add(wled);
+                                            _wledLights = new BindingList<WLED>(_wledLights.OrderBy(wled => wled.Name).Distinct().ToList());
 
-                                        DeviceDiscovered?.Invoke(this, wled);
+                                            // Update the DataSource property of the listbox control to reflect the changes in the _wledLights list.
+                                            listbox_wledlights.DataSource = null;
+                                            listbox_wledlights.DataSource = _wledLights;
+                                            listbox_wledlights.DisplayMember = "Name";
+
+                                            // Select the stored selected item in the listbox.
+                                            var selectedItem = this.settings.SelectedWled;
+                                            if (selectedItem != null && selectedItem.Ip == wled.Ip && selectedItem.Port == wled.Port)
+                                            {
+                                                listbox_wledlights.SelectedItem = wled;
+                                            }
+
+                                            DeviceDiscovered?.Invoke(this, wled);
+                                        }
                                     }
+                                    catch
+                                    {
+                                        //wasn't a wled device
+                                    }
+                                    // use the name variable to access the name of the device
                                 }
-                                catch
-                                {
-                                    //wasn't a wled device
-                                }
-                                // use the name variable to access the name of the device
+                            }
+                            catch (Exception ex)
+                            {
+                                //wasn't a wled device
+                                Log.Error("Exception on Discovery of WLED lights {ex}", ex.Message.ToString());
                             }
                         }
-                        catch (Exception ex)
-                        {
-                            //wasn't a wled device
-                            Log.Error("Exception on Discovery of WLED lights {ex}", ex.Message.ToString());
-                        }
                     }
-                }
-                settings.WledDevices = WledLights;
-                settings.Save();
+                    settings.WledDevices = WledLights;
+                    settings.Save();
 
+                }
+                catch (Exception ex)
+                {
+                    Log.Information("Exception on Discovery of WLED lights {ex}", ex.Message.ToString());
+                }
             }
             catch (Exception ex)
             {
-                Log.Information("Exception on Discovery of WLED lights {ex}", ex.Message.ToString());
+                // Handle any exceptions
+            }
+            finally
+            {
+                isDiscovering = false;
+                toolStripStatusLabel1.Text = "Stopped Discovering Lights";
             }
         }
 
         private void listbox_wledlights_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (listbox_wledlights.SelectedItem is WLED selectedWled)
+            {
+                this.settings.SelectedWled = selectedWled;
+                this.settings.Save();
+            }
             settings.WledDevices = WledLights;
             settings.Save();
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            isDiscovering = false;
+            toolStripStatusLabel1.Text = "Stopped Discovering Lights";
         }
     }
 }
